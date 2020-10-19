30e98bf475d4fd8d6d5a5a5963fb71a67c7d9a17
yosh1 <yoppy1125yoppy@gmail.com>
2020-10-19 14:13:21 +0900

    Fix rule

--- ex2.rb
+++ ex2.rb
@@ -1,7 +1,7 @@
 # 引数として数値を1個受け取り、それが素数なら true そうでなければ false を返すメソッド(関数)を作れ。
 # 次に、 gets.to_i で数値を入力し、上の関数を呼び出して素数かどうか判定して、素数ならYes、素数でなければNoと出力するようにせよ。
 
-def primeCheck(n)
+def prime_check(n)
   start = 2
   n.times do 
     if((n % start) == 0 && (n != start))
@@ -13,4 +13,4 @@ def primeCheck(n)
   return "Yes"
 end
 
-puts primeCheck(gets.to_i)
+puts prime_check(gets.to_i)
